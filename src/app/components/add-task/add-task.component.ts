7fa6e5209a35d5ce84c2af67690ad211e0961694
PAWAR-AKSHAY <pawar.pawar1@gmail.com>
2021-07-13 19:38:46 +0530

    Feature - Adds ui service - Adds toggle functionality

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -8,11 +10,18 @@ import { Task } from 'src/app/Task';
 })
 export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
   
-  constructor() { }
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService.onToggle().subscribe(
+      value => this.showAddTask = value
+    );
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title:string = 'Task Tracker';
-  constructor() { }
+  showAddTask: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(
+      value => this.showAddTask = value
+      );
+   }
 
   ngOnInit(): void {
   }
 
-  onClick(){
-    console.log('Add');
-  }
+  toggleAddTask(){
+  this.uiService.toggleAddTask();
+ }
 }
